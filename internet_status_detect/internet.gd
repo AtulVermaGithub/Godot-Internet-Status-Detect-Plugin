e02707c5775ecbd4a9cf3678310294520e5e42a8
Atul Verma <100815053+AtulVermaGithub@users.noreply.github.com>
2024-08-31 13:04:28 +0530

    Update internet.gd

--- internet_status_detect/internet.gd
+++ internet_status_detect/internet.gd
@@ -1,8 +1,7 @@
-@tool
 extends Node
 
 var _http_request: HTTPRequest
-@export var status: bool
+var status: bool
 func _ready() -> void:
 	make_http_request()
 	pass
@@ -12,15 +11,12 @@ func _process(delta: float) -> void:
 	
 func get_status():
 	status = false
-	#get_tree().reload_current_scene()
 	_http_request.queue_free()
 	make_http_request()
 
 func connection_status():
-	#print("status")
 	#var state = 0
 	var state = _http_request.get_http_client_status()
-	print(state)
 	if state in [3, 5, 6, 7]:
 		status = true
 	
